62f18a14f711b94758888f2df9241ef47f0067e3
Hugo Jorge Macias <huma001@ess.ais.ac.nz>
2023-02-20 15:24:50 +1300

    Hola

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ public class Test
 	{
 	int tableNo=0;
 	for(int i = 1; i <= 5; i++)
-	  System.out.println(tableNo + " Hugo* " +
+	  System.out.println(tableNo + " * " +
 			 i + " = " + tableNo *i);
 	}
 }
